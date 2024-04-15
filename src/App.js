02088b09127973a80bbcb678b7c420a56c096277
rjnazareno <157678475+rjnazareno@users.noreply.github.com>
2024-04-15 19:46:43 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Nazareno({ display }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Nazareno">
+      {display}
     </div>
   );
 }
 
+function App() {
+  return (
+    <div className="App">
+      <Nazareno display="hello world" />
+      <Nazareno2 />
+    </div>
+  );
+}
+
+function Nazareno2() {
+  return (
+    <div className="Nazareno2">
+      <div className="top">
+        <div className="tl">
+          <div className="item1">1</div>
+          <div className="item2">2</div>
+        </div>
+        <div className="tr">
+          <div className="item3">3</div>
+          <div className="item4">4</div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="bl">
+          <div className="item5">5</div>
+          <div className="item6">6</div>
+        </div>
+        <div className="br">
+          <div className="item7">7</div>
+          <div className="item8">8</div>
+        </div>
+      </div>
+    </div>
+  );
+}
 export default App;
